4d4e35d9480b1dbc738b4f0d4fec17ae9b0c4661
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-06-14 13:04:47 +0600

    added clickable sort and categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/modules/categories.module.scss";
 
 const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <ul className={styles.categories__list}>
-      <li className={styles.active}>ВСЕ</li>
-      <li className={styles.categories__item}>G-SHOCK</li>
-      <li className={styles.categories__item}>BABY-G</li>
-      <li className={styles.categories__item}>EDIFICE</li>
-      <li className={styles.categories__item}>SHEEN</li>
-      <li className={styles.categories__item}>VINTAGE</li>
+      <li
+        onClick={() => handleClick(0)}
+        className={activeIndex === 0 ? styles.active : styles.item}
+      >
+        ВСЕ
+      </li>
+      <li
+        onClick={() => handleClick(1)}
+        className={activeIndex === 1 ? styles.active : styles.item}
+      >
+        G-SHOCK
+      </li>
+      <li
+        onClick={() => handleClick(2)}
+        className={activeIndex === 2 ? styles.active : styles.item}
+      >
+        BABY-G
+      </li>
+      <li
+        onClick={() => handleClick(3)}
+        className={activeIndex === 3 ? styles.active : styles.item}
+      >
+        EDIFICE
+      </li>
+      <li
+        onClick={() => handleClick(4)}
+        className={activeIndex === 4 ? styles.active : styles.item}
+      >
+        SHEEN
+      </li>
+      <li
+        onClick={() => handleClick(5)}
+        className={activeIndex === 5 ? styles.active : styles.item}
+      >
+        VINTAGE
+      </li>
     </ul>
   );
 };
--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -5,11 +5,14 @@ import styles from "../styles/modules/sort.module.scss";
 
 const Sort = () => {
   const [modal, setModal] = useState(false);
+  const [indexActive, setIndexActive] = useState(0);
 
- 
+  const handleClick = (index) => {
+    setIndexActive(index);
+  };
 
   return (
-    <div className={styles.sort__container} onClick={() =>  setModal(!modal)}>
+    <div className={styles.sort__container} onClick={() => setModal(!modal)}>
       <div className={styles.sort__label}>
         <BsFillCaretUpFill />
         <p>Сортировка по:</p>
@@ -18,12 +21,42 @@ const Sort = () => {
       {modal && (
         <div className={styles.sort__popup}>
           <ul>
-            <li className={styles.active}>популярности (возр)</li>
-            <li>популярности (убыв)</li>
-            <li>цене (возр)</li>
-            <li>цене (убыв)</li>
-            <li>алфавиту (возр)</li>
-            <li>алфавиту (убыв)</li>
+            <li
+              onClick={() => handleClick(0)}
+              className={indexActive === 0 ? styles.active : ""}
+            >
+              популярности (возр)
+            </li>
+            <li
+              onClick={() => handleClick(1)}
+              className={indexActive === 1 ? styles.active : ""}
+            >
+              популярности (убыв)
+            </li>
+            <li
+              onClick={() => handleClick(2)}
+              className={indexActive === 2 ? styles.active : ""}
+            >
+              цене (возр)
+            </li>
+            <li
+              onClick={() => handleClick(3)}
+              className={indexActive === 3 ? styles.active : ""}
+            >
+              цене (убыв)
+            </li>
+            <li
+              onClick={() => handleClick(4)}
+              className={indexActive === 4 ? styles.active : ""}
+            >
+              алфавиту (возр)
+            </li>
+            <li
+              onClick={() => handleClick(5)}
+              className={indexActive === 5 ? styles.active : ""}
+            >
+              алфавиту (убыв)
+            </li>
           </ul>
         </div>
       )}
